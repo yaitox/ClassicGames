6a7c739a44a7043298fc9bf330030595910546c6
yaitox <pedro.bohorquez.2019@upb.edu.co>
2024-04-04 12:01:29 -0500

    Chess: rename board file and update Board class.

---
+++ Chess/ChessBoard.h
@@ -0,0 +1,40 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#include "Field.h"
+
+#define MAX_COLUMNS 8
+#define MAX_ROWS	8
+
+class Board
+{
+public:
+	static Board* instance()
+	{
+		static Board instance;
+		return &instance;
+	}
+
+	Board(Board const&) = delete;
+	void operator=(Board const&) = delete;
+
+	void Initialize();
+	void InitializePawns();
+	void InitializeKnights();
+	void InitializeQueens();
+	void InitializeBishops();
+	void InitializeRooks();
+	void InitializeKings();
+
+	void Test();
+
+	void Update();
+
+private:
+	Board() { }
+	Field _board[MAX_ROWS][MAX_COLUMNS];
+};
+
+#define sChessBoard Board::instance()
+
+#endif
