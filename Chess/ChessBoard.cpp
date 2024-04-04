d8b480f483f74438388da534c2c5176e3c17ba2f
yaitox <pedro.bohorquez.2019@upb.edu.co>
2024-04-04 18:51:57 -0500

    Chess: Implement InitializeKnights function.

--- Chess/ChessBoard.cpp
+++ Chess/ChessBoard.cpp
@@ -42,7 +42,61 @@ void Board::InitializePawns()
 	}
 }
 
+void Board::InitializeKnights()
+{
+	// Black knights
+	// Left b8
+	Piece* blackKnightLeft = new Piece(Team::Black, PieceType::Knight);
+	Position blackKnightLeftPosition;
+	blackKnightLeftPosition.X = 1;
+	blackKnightLeftPosition.Y = 0;
+
+	Field fieldBlackKnightLeft;
+	fieldBlackKnightLeft.FieldColor = FieldColor::Black;
+	fieldBlackKnightLeft.Piece = blackKnightLeft;
+	fieldBlackKnightLeft.Position = blackKnightLeftPosition;
+	_board[fieldBlackKnightLeft.Position.Y][fieldBlackKnightLeft.Position.X] = fieldBlackKnightLeft;
+
+	// Right g8
+	Piece* blackKnightRight = new Piece(Team::Black, PieceType::Knight);
+	Position blackKnightRightPosition;
+	blackKnightRightPosition.X = 6;
+	blackKnightRightPosition.Y = 0;
+
+	Field fieldBlackKnightRight;
+	fieldBlackKnightRight.FieldColor = FieldColor::White;
+	fieldBlackKnightRight.Piece = blackKnightRight;
+	fieldBlackKnightRight.Position = blackKnightRightPosition;
+	_board[fieldBlackKnightRight.Position.Y][fieldBlackKnightRight.Position.X] = fieldBlackKnightRight;
+
+	// White knights
+	// Left b1
+	Piece* whiteKnightLeft = new Piece(Team::White, PieceType::Knight);
+	Position whiteKnightLeftPosition;
+	whiteKnightLeftPosition.X = 1;
+	whiteKnightLeftPosition.Y = 7;
+
+	Field fieldWhiteKnightLeft;
+	fieldWhiteKnightLeft.FieldColor = FieldColor::White;
+	fieldWhiteKnightLeft.Piece = whiteKnightLeft;
+	fieldWhiteKnightLeft.Position = whiteKnightLeftPosition;
+	_board[fieldWhiteKnightLeft.Position.Y][fieldWhiteKnightLeft.Position.X] = fieldWhiteKnightLeft;
+
+	// Right g1
+	Piece* whiteKnightRight = new Piece(Team::White, PieceType::Knight);
+	Position whiteKnightRightPosition;
+	whiteKnightRightPosition.X = 6;
+	whiteKnightRightPosition.Y = 7;
+
+	Field fieldWhiteKnightRight;
+	fieldWhiteKnightRight.FieldColor = FieldColor::Black;
+	fieldWhiteKnightRight.Piece = whiteKnightRight;
+	fieldWhiteKnightRight.Position = whiteKnightRightPosition;
+	_board[fieldWhiteKnightRight.Position.Y][fieldWhiteKnightRight.Position.X] = fieldWhiteKnightRight;
+}
+
 void Board::Initialize()
 {
 	InitializePawns();
+	InitializeKnights();
 }
